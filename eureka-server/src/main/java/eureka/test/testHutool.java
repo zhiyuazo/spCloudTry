f714963db884ac517fae6810d9e61fa3e500317e
zhiyuazo <zhiyuazo>
2020-01-14 23:13:18 +0800

    start...

---
+++ eureka-server/src/main/java/eureka/test/testHutool.java
@@ -0,0 +1,11 @@
+package eureka.test;
+
+import cn.hutool.json.JSONObject;
+
+public class testHutool {
+	public static void main(String[] args) {
+		JSONObject  jo = new JSONObject();
+		System.out.println(jo.toString());
+	}
+
+}
